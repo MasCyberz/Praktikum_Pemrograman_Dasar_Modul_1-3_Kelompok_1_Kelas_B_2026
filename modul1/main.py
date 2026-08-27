0327f689fbe4f84377c11a9a67355e9ecbed6a0c
MasCyberz <dimasalifardiansyahh@gmail.com>
2026-08-27 09:07:18 +0700

    Modul 1 dan Modul 2

---
+++ modul1/main.py
@@ -0,0 +1,10 @@
+nama = input("Masukan nama: ")
+umur = int(input("Masukan umur mu: "))
+    
+def sapa(nama, umur):
+    print("Halo", nama, "umur mu", umur)
+    print("Tahun lahirmu", 2026 - umur)
+
+sapa(nama, umur)
+
+    
